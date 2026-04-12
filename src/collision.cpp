f40ce201b5acd80df27ccad4f43728292c249d86
Rin <84992619+Rin-coder69@users.noreply.github.com>
2026-04-12 14:14:41 -0600

    Resolve Contacts

--- src/collision.cpp
+++ src/collision.cpp
@@ -48,3 +48,29 @@ void SeparateContacts(std::vector<Contact>& contacts)
 		contact.bodyB->position = contact.bodyB->position + (separation * contact.bodyB->inverseMass);
 	}
 }
+
+void ResolveContacts(std::vector<Contact>& contacts)
+{
+    for (auto& contact : contacts)
+    {
+        // compute relative velocity
+        Vector2 rv = contact.bodyA->Velocity - contact.bodyB->Velocity;
+        // project relative velocity onto the contact normal
+        float nv = Vector2DotProduct(rv, contact.normal);
+
+        // skip if bodies are separating
+        if (nv > 0) continue;
+
+        // total inverse mass = (1/mA + 1/mB)
+        float totalInverseMass = contact.bodyA->inverseMass + contact.bodyB->inverseMass;
+        // impulse scalar = -(1 + restitution) * vn / (1/mA + 1/mB)
+        float impulseMagnitude = -(1 + contact.restitution) * nv / totalInverseMass;
+
+        // impulse vector along contact normal
+        Vector2 impulse = Vector2Scale(contact.normal, impulseMagnitude);
+
+        // apply equal and opposite impulses
+        contact.bodyA->AddForce(impulse, ForceMode::Impulse);
+        contact.bodyB->AddForce(Vector2Negate(impulse), ForceMode::Impulse);
+    }
+}
